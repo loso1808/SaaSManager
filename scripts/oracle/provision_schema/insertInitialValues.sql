302e146c91ae463025eb881949c27de31935fd9f
Adam Thomas <adam@teselagen.com>
2016-05-31 14:23:10 -0400

    adding initial values script

---
+++ scripts/oracle/provision_schema/insertInitialValues.sql
@@ -0,0 +1,19 @@
+
+
+insert into "user" ("id","username","firstName","lastName") values (0, 'app','app','server');
+
+Insert into "role" ("id","name","description") values (4,'viewer','Can view data in the shared Peony repo');
+Insert into "role" ("id","name","description") values (5,'contributor','Can add data into the shared Peony repo');
+Insert into "role" ("id","name","description") values (1,'editor','Can edit their own non-shared data');
+Insert into "role" ("id","name","description") values (2,'admin','Full access to all app functionality.');
+Insert into "role" ("id","name","description") values (3,'user_manager','Can manage users and assign roles.');
+
+insert into "user" ("id","username","firstName","lastName", "password", "activated", "activeFlag") 
+values (1, 'admin','Admin','User', '$2a$10$a3BOTytYHBko.I4aKpAPR.4b6Sr3s93mUH.UNwMyDGhdxo42sm.GW', 1, 1);
+
+insert into "user_role" ("user_id", "role_id") values (1, 2);
+
+commit;
+
+exit;
+
