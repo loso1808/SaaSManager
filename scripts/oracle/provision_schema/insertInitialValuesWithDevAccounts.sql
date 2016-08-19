6b84d37be4f5e313c6bdec5aa14a9887d3b9e7b7
Sam DeNicola <samdenicola@teselagen.com>
2016-08-18 17:00:13 -0700

    Adds Mike and Nathan to initialValues script

--- scripts/oracle/provision_schema/insertInitialValuesWithDevAccounts.sql
+++ scripts/oracle/provision_schema/insertInitialValuesWithDevAccounts.sql
@@ -1,5 +1,3 @@
-
-
 insert into "user" ("id","username","firstName","lastName") values (0, 'app','app','server');
 
 Insert into "role" ("id","name","description") values (4,'viewer','Can view data in the shared Peony repo');
@@ -42,6 +40,10 @@ insert into "user" ("id","username","firstName","lastName", "password", "activat
 values (16, 'tgreen','Taoh','Green', '$2a$10$a3BOTytYHBko.I4aKpAPR.4b6Sr3s93mUH.UNwMyDGhdxo42sm.GW', 1, 1);
 insert into "user" ("id","username","firstName","lastName", "password", "activated", "activeFlag") 
 values (17, 'tgreen2','Taoh','Green', '$2a$10$a3BOTytYHBko.I4aKpAPR.4b6Sr3s93mUH.UNwMyDGhdxo42sm.GW', 1, 1);
+insert into "user" ("id","username","firstName","lastName", "password", "activated", "activeFlag") 
+values (18, 'mikefero','Mike','Fero', '$2a$10$a3BOTytYHBko.I4aKpAPR.4b6Sr3s93mUH.UNwMyDGhdxo42sm.GW', 1, 1);
+insert into "user" ("id","username","firstName","lastName", "password", "activated", "activeFlag") 
+values (19, 'njhillson','Nathan','Hillson', '$2a$10$a3BOTytYHBko.I4aKpAPR.4b6Sr3s93mUH.UNwMyDGhdxo42sm.GW', 1, 1);
 
 insert into "user_role" ("user_id", "role_id") values (1, 2);
 insert into "user_role" ("user_id", "role_id") values (2, 2);
@@ -60,5 +62,7 @@ insert into "user_role" ("user_id", "role_id") values (14, 2);
 insert into "user_role" ("user_id", "role_id") values (15, 2);
 insert into "user_role" ("user_id", "role_id") values (16, 2);
 insert into "user_role" ("user_id", "role_id") values (17, 2);
+insert into "user_role" ("user_id", "role_id") values (18, 2);
+insert into "user_role" ("user_id", "role_id") values (19, 2);
 
 commit;
