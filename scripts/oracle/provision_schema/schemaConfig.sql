952a6863425fcce199f5ac3aec3fbe5ed2d978a2
Adam Thomas <adam@teselagen.com>
2016-09-12 13:44:38 -0400

    switched schemaConfig to use new snapshot

--- scripts/oracle/provision_schema/schemaConfig.sql
+++ scripts/oracle/provision_schema/schemaConfig.sql
@@ -1,8 +1,8 @@
 --DEFINE the following at the top of your provisionShema script or pass it in as a parameter 
 
 DEFINE SCHEMA_CODE = 'RC_1_9_0';
-DEFINE AWS_RDS = 0;
-DEFINE BASE_SCHEMA_FILE = 'rc_1_8_0_snapshot_owner_schema.sql';
+DEFINE AWS_RDS = 1;
+DEFINE BASE_SCHEMA_FILE = 'rc_1_9_0_snapshot_owner_schema.sql';
 
 --@@provisionSchemaOnly.sql;
 --@@schemaDefines.sql;
